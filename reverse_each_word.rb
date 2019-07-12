aa6363bb00c9099f58ac6ea876238bdfac10a922
Barrette Banner <barrettebanner@gmail.com>
2019-07-12 01:36:07 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(string)
+ #turn string into Array
+ array = string.split(" ")
+ #iterate over each word 
+ new_array = []
+ array.each do |word|
+   new_array.push(word.reverse)
+ end    
+ new_string = new_array.join(" ")
+ return new_string
+ #reverse each word 
+ #turn array into string 
+end 
+
+
